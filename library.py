ad440f17d1502a0f27b64a0a7e1b5df2d2a72bd3
Michelle Brown <michelle.brown2@bison.howard.edu>
2019-03-27 21:23:59 -0700

    Finished proxy phew

---
+++ library.py
@@ -0,0 +1,126 @@
+"""A set of libraries that are useful to both the proxy and regular servers."""
+
+# This code uses Python 2.7. These imports make the 2.7 code feel a lot closer
+# to Python 3. (They're also good changes to the language!)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# THe Python socket API is based closely on the Berkeley sockets API which
+# was originally written for the C programming language.
+#
+# https://en.wikipedia.org/wiki/Berkeley_sockets
+#
+# The API is more flexible than you need, and it does some quirky things to
+# provide that flexibility. I recommend tutorials instead of complete
+# descriptions because those can skip the archaic bits. (The API was released
+# more than 35 years ago!)
+import socket
+
+import time
+
+# Read this many bytes at a time of a command. Each socket holds a buffer of
+# data that comes in. If the buffer fills up before you can read it then TCP
+# will slow down transmission so you can keep up. We expect that most commands
+# will be shorter than this.
+COMMAND_BUFFER_SIZE = 256
+
+
+def CreateServerSocket(port):
+  """Creates a socket that listens on a specified port.
+
+  Args:
+    port: int from 0 to 2^16. Low numbered ports have defined purposes. Almost
+        all predefined ports represent insecure protocols that have died out.
+  Returns:
+    An socket that implements TCP/IP.
+  """
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  addr = ('localhost', port)
+  sock.bind(addr)
+  sock.listen(10)
+  return sock
+
+def ConnectClientToServer(server_sock):
+    # Wait until a client connects and then get a socket that connects to the
+    # client.
+  return server_sock.accept()
+
+def CreateClientSocket(server_addr, port):
+  """Creates a socket that connects to a port on a server."""
+  client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client_sock.connect((server_addr, port))
+  return client_sock
+
+
+def ReadCommand(sock):
+  """Read a single command from a socket. The command must end in newline."""
+  command = ''
+  while not command or command[-1] != '\n':
+      command = sock.recv(COMMAND_BUFFER_SIZE)
+  return command.strip()
+
+def ParseCommand(command):
+  """Parses a command and returns the command name, first arg, and remainder.
+
+  All commands are of the form:
+      COMMAND arg1 remaining text is called remainder
+  Spaces separate the sections, but the remainder can contain additional spaces.
+  The returned values are strings if the values are present or `None`. Trailing
+  whitespace is removed.
+
+  Args:
+    command: string command.
+  Returns:
+    command, arg1, remainder. Each of these can be None.
+  """
+  args = command.strip().split(' ')
+  command = None
+  if args:
+    command = args[0]
+  arg1 = None
+  if len(args) > 1:
+    arg1 = args[1]
+  remainder = None
+  if len(args) > 2:
+    remainder = ' '.join(args[2:])
+  return command, arg1, remainder
+
+
+class KeyValueStore(object):
+  """A dictionary of strings keyed by strings.
+  The values can time out once they get sufficiently old. Otherwise, this
+  acts much like a dictionary.
+  """
+  def __init__(self):
+    self.store = dict()
+
+  def GetValue(self, key, max_age_in_sec=None):
+    """Gets a cached value or `None`.
+    Values older than `max_age_in_sec` seconds are not returned.
+    Args:
+      key: string. The name of the key to get.
+      max_age_in_sec: float. Maximum time since the value was placed in the
+        KeyValueStore. If not specified then values do not time out.
+    Returns:
+      None or the value.
+    """
+    # Check if we've ever put something in the cache.
+    if key in self.store.keys():
+      return self.store[key]
+    else:
+      return None
+
+
+  def StoreValue(self, key, value):
+    """Stores a value under a specific key.
+    Args:
+      key: string. The name of the value to store.
+      value: string. A value to store.
+    """
+    self.store[key] = value
+
+
+  def Keys(self):
+    """Returns a list of all keys in the datastore."""
+    return list(self.store.keys())
---
+++ proxy.py
@@ -0,0 +1,106 @@
+""A proxy server that forwards requests from one port to another server.
+To run this using Python 2.7:
+% python proxy.py
+It listens on a port (`LISTENING_PORT`, below) and forwards commands to the
+server. The server is at `SERVER_ADDRESS`:`SERVER_PORT` below.
+"""
+
+# This code uses Python 2.7. These imports make the 2.7 code feel a lot closer
+# to Python 3. (They're also good changes to the language!)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import library
+
+# Where to find the server. This assumes it's running on the smae machine
+# as the proxy, but on a different port.
+SERVER_ADDRESS = 'localhost'
+SERVER_PORT = 7777
+
+# The port that the proxy server is going to occupy. This could be the same
+# as SERVER_PORT, but then you couldn't run the proxy and the server on the
+# same machine.
+LISTENING_PORT = 8888
+
+# Cache values retrieved from the server for this long.
+MAX_CACHE_AGE_SEC = 60.0  # 1 minute
+
+
+def ForwardCommandToServer(command, server_addr, server_port):
+  """Opens a TCP socket to the server, sends a command, and returns response.
+  Args:
+    command: A single line string command with no newlines in it.
+    server_addr: A string with the name of the server to forward requests to.
+    server_port: An int from 0 to 2^16 with the port the server is listening on.
+  Returns:
+    A single line string response with no newlines.
+  """
+  client_socket = library.CreateClientSocket(server_addr, server_port)
+  client_socket.send(command + '\n')
+  response = library.ReadCommand(client_socket)
+  client_socket.close()
+  return response + '\n'
+
+
+
+def CheckCachedResponse(command_line, cache):
+  command, name, text = library.ParseCommand(command_line)
+  # Update the cache for PUT commands but also pass the traffic to the server.
+  if command == "PUT":
+     cache.StoreValue(name, text)
+     ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
+     return None
+  if command == "GET":
+      #send to server
+      if name in cache:
+          return cache[name]
+      else:
+          #use forward command to server and store command in cache
+          ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
+
+
+def ProxyClientCommand(sock, server_addr, server_port, cache):
+  """Receives a command from a client and forwards it to a server:port.
+  A single command is read from `sock`. That command is passed to the specified
+  `server`:`port`. The response from the server is then passed back through
+  `sock`.
+  Args:
+    sock: A TCP socket that connects to the client.
+    server_addr: A string with the name of the server to forward requests to.
+    server_port: An int from 0 to 2^16 with the port the server is listening on.
+    cache: A KeyValueStore object that maintains a temorary cache.
+    max_age_in_sec: float. Cached values older than this are re-retrieved from
+      the server.
+  """
+  # Read the command line and parse it.
+  command_line = library.ReadCommand(sock)
+  command, name, value = library.ParseCommand(command_line)
+  # Determine the type of command.
+  if command == 'PUT':
+    response = ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
+    cache.StoreValue(name, response)
+  elif (command == 'GET'):
+    response = cache.GetValue(name)
+    if response == None:
+      response = ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
+  else:
+    response = ForwardCommandToServer(command_line, SERVER_ADDRESS, SERVER_PORT)
+
+  sock.send(response)
+
+def main():
+  # Listen on a specified port...
+  server_socket = library.CreateServerSocket(LISTENING_PORT)
+  cache = library.KeyValueStore()
+  # Accept incoming commands indefinitely.
+  while True:
+    # Wait until a client connects and then get a socket that connects to the
+    # client.
+    client_socket, (address, port) = library.ConnectClientToServer(server_socket)
+    print('Received connection from %s:%d' % (address, port))
+    ProxyClientCommand(client_socket, SERVER_ADDRESS, SERVER_PORT,
+                       cache)
+    client_socket.close()
+
+main()
